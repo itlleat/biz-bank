2260b014810fb2a074ca821c501867b63d994a93
itlleat <scott.schulman84@gmail.com>
2023-03-22 18:21:05 -0600

    close need to promisify switch cases

---
+++ routes/connection.js
@@ -0,0 +1,15 @@
+// const mysql = require('mysql2/promise');
+
+// // create the connection
+// const connection = async () => {
+//   const createConnection = await mysql.connection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'your_new_password',
+//     database: 'departments_db'
+//   });
+//   return createConnection;
+// };
+
+// module.exports = connection;
+
